f261f0d387250ba124ab747baa9c8e1d53c2c8c5
JoaoFelipeSantana <j155c137@gmail.com>
2023-11-03 13:16:47 -0300

    [feat] - Criando a DTO de entrada de dados de produtos

---
+++ src/main/java/com/joao/application/DTOs/ProductDTOinput.java
@@ -0,0 +1,53 @@
+package com.joao.application.DTOs;
+
+public class ProductDTOinput {
+    private String name_product;
+    private String desription_product;
+    private float price;
+    private int amount;
+    private int stock_min;
+    private boolean status_product;
+
+
+    // ===== MÉTODO CONTRUTOR =====
+    public ProductDTOinput(String name_product,
+                           String desription_product,
+                           float price,
+                           int amount,
+                           int stock_min,
+                           boolean status_product) {
+        this.name_product = name_product;
+        this.desription_product = desription_product;
+        this.price = price;
+        this.amount = amount;
+        this.stock_min = stock_min;
+        this.status_product = status_product;
+    }
+
+    // ===== MÉTODOS ACESSORES =====
+
+
+    public String getName_product() {
+        return name_product;
+    }
+
+    public String getDesription_product() {
+        return desription_product;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getStock_min() {
+        return stock_min;
+    }
+
+    public boolean isStatus_product() {
+        return status_product;
+    }
+}
