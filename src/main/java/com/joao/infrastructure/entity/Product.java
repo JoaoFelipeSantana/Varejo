ec8a7e1a994ba44c361f17cf3142bc606656b11e
JoaoFelipeSantana <j155c137@gmail.com>
2023-11-03 13:10:32 -0300

    [feat] - Criando a entidade produto

---
+++ src/main/java/com/joao/infrastructure/entity/Product.java
@@ -0,0 +1,67 @@
+package com.joao.infrastructure.entity;
+
+public class Product {
+    // ===== ATRIBUTOS =====
+    private String name_product;
+    private String description_product;
+    private float price;
+    private int amount;
+    private int stock_min;
+    private String dtcreate;
+    private String dtupdate;
+    private boolean status_product;
+
+    // ===== MÉTODO CONTRUTOR =====
+
+
+    public Product(String name_product,
+                   String description_product,
+                   float price, int amount,
+                   int stock_min,
+                   String dtcreate,
+                   String dtupdate,
+                   boolean status_product) {
+
+        this.name_product = name_product;
+        this.description_product = description_product;
+        this.price = price;
+        this.amount = amount;
+        this.stock_min = stock_min;
+        this.dtcreate = dtcreate;
+        this.dtupdate = dtupdate;
+        this.status_product = status_product;
+    }
+
+    // ===== MÉTODOS ACESSORES =====
+    public String getName_product() {
+        return name_product;
+    }
+
+    public String getDescription_product() {
+        return description_product;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getStock_min() {
+        return stock_min;
+    }
+
+    public String getDtcreate() {
+        return dtcreate;
+    }
+
+    public String getDtupdate() {
+        return dtupdate;
+    }
+
+    public boolean isStatus_product() {
+        return status_product;
+    }
+}
